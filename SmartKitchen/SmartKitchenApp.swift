747ee4a7bbea0aca9b50242e6312a8292bbf3062
Pythm441 <Abdulrahmanmetawea6@gmail.com>
2023-08-09 12:45:17 +0400

    Initial Commit

---
+++ SmartKitchen/SmartKitchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartKitchenApp.swift
+//  SmartKitchen
+//
+//  Created by Abdelrahman Hesham on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SmartKitchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
