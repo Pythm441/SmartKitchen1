895517ce8da51c0a8bc738e2c89749a98eb5026d
Pythm441 <Abdulrahmanmetawea6@gmail.com>
2023-08-20 13:43:07 +0400

    added file assets

---
+++ SmartKitchen/HomeScreen.swift
@@ -0,0 +1,27 @@
+//
+//  HomeScreen.swift
+//  SmartKitchen
+//
+//  Created by Abdelrahman Hesham on 20/08/2023.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        VStack{
+            HStack{
+               
+            }
+            HStack{
+                
+            }
+        }
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
