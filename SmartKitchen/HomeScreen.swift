622604c539720b3eb9174b25cc7e76ee2226dac9
Pythm441 <Abdulrahmanmetawea6@gmail.com>
2023-08-20 13:46:54 +0400

    add logo to the home screen

--- SmartKitchen/HomeScreen.swift
+++ SmartKitchen/HomeScreen.swift
@@ -11,7 +11,7 @@ struct HomeScreen: View {
     var body: some View {
         VStack{
             HStack{
-               
+               Image("logo")
             }
             HStack{
                 
