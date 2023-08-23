7b4901380c26410462cb82a75d5bbd0f6b54794f
9cvi <aw@ar.ae>
2023-08-23 12:02:46 +0400

    Test

--- SmartKitchen/Models/RecipeModel.swift
+++ SmartKitchen/Models/RecipeModel.swift
@@ -6,22 +6,42 @@
 //
 
 import Foundation
+import SwiftUI
 
- 
-
-
-
-struct Response: Codable {
-    let results: myResults
-    let offset: Int
-    let number: Int
-    let totalResults: Int
-
+class ViewModel: ObservableObject {
+    @Published private(set) var myResult: [myResults] = []
+    
+    init() {
+    
+    func fetch() {
+        guard let url = URL(string: "https://api.spoonacular.com/recipes/complexSearch?apiKey=3dcc7436a1dd43e1a71b267740fafacc") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { [weak self]
+            data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do {
+                let myResult = try JSONDecoder().decode([myResults].self , from: data)
+                DispatchQueue.main.async {
+                    self?.myResult = myResult
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+}
 }
 
-struct myResults: Codable {
+
+struct myResults: Hashable, Codable {
     let id : UInt
     let title : String
     let image : String
     let imageType : String
+    let number : Int
 }
